8de181c37e494b76c0741e2080a7655246ea1fee
Arjan-7 <gautamaarjam7@gmail.com>
2026-04-06 11:30:22 +0545

    change done

--- project.py
+++
@@ -1,8 +0,0 @@
-import streamlit as sl
-sl.title("Hi, I am streamlit web app.")
-sl.subheader("Hi, I am your subheader.")
-sl.markdown("* Hello! world.")
-sl.markdown("[Facebook](https://www.facebook.com)")
-sl.info("Information details of user:")
-sl.error("Wrong password!")
-sl.write(range(50))
